117fcac8ad44800cbf6dbd7b208620398594670c
Sergey Beketov <sergey_2993@mail.ru>
2017-02-16 16:15:09 +0300

    gitignore changes

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ const $ = require('jquery');
 const removeButton = '<button id="remove-todo" class="remove-button">Remove</button>';
 require('./app.scss');
 $(() => {
-		$(document).on('click', '#add-todo', () => {
-			$('#warn').hide();
-			let title = $('#title').val();
-			if (title == '') {
-				$('#warn').show();
-				return;
-			} 
-			let todos = $('#todos');
-			let firstRemoved = todos.find('li.line-through:first');
-			if(firstRemoved.length > 0) {
-				firstRemoved.before('<li><span>' + title + '</span>' + removeButton + '</li>');
-			} else {
-				todos.append('<li><span>' + title + '</span>' + removeButton + '</li>');
-			}
-			$('#title').val('');
-		});
-		
-		$(document).on('click', '#remove-todo', (event) => {
-			let last = $('#todos').find('li:last-child');
-			let btn = $(event.target);
-			let toMove = btn.parent('li');			
-			toMove.addClass('line-through');
-			toMove.insertAfter(last);
-			btn.hide();
-		});
-	}
-);
+    $(document).on('click', '#add-todo', () => {
+        let title = $('#title');
+        let warn = $('#warn');
+        warn.hide();
+        if (title.val() == '') {
+            warn.show();
+            return;
+        }
+        let todos = $('#todos');
+        let firstRemoved = todos.find('li.line-through:first');
+        let newTodo = '<li><span>' + title.val() + '</span>' + removeButton + '</li>';
+        if (firstRemoved.length > 0) {
+            firstRemoved.before(newTodo);
+        } else {
+            todos.append(newTodo);
+        }
+        title.val('');
+    });
+
+    $(document).on('click', '#remove-todo', (event) => {
+        let last = $('#todos').find('li:last-child');
+        let btn = $(event.target);
+        let toMove = btn.parent('li');
+        toMove.addClass('line-through');
+        toMove.insertAfter(last);
+        btn.hide();
+    });
+});
